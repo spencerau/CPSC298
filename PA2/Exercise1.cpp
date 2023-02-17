b752bd5806990753f5ba1bbffe5dd980c923f00a
Spencer Au <spencerau96@gmail.com>
2023-02-17 02:52:58 -0800

    Initial commit

---
+++ PA2/Exercise1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Exercise1 {
+public:
+    double findFreq(double refFreq, int oct, double k);
+
+private:
+};
+
+int main() {
+
+}
+
+double findFreq(double refFreq, int oct, double k) {
+
+}
---
+++ Proj_HelloCpp/hello_cin.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std; 
+
+int main() 
+{ 
+   string strMessage; 
+   cout << "Please type a single word message" << endl; 
+   cin >> strMessage; 
+   cout << "Message: " << strMessage << endl; 
+   return 0; 
+}
